33f8cefcdfbae32c4fc7efd9a17313d190be3c3d
Colin Brooks <brookstco@gmail.com>
2021-10-18 22:09:35 -0400

    Off, On, and Toggle seperation
    Allows finer control from automation

--- app/src/main/java/com/catly/oledsaver/features/utils/FloatingWindowServiceActivity.kt
+++ app/src/main/java/com/catly/oledsaver/features/utils/FloatingWindowServiceActivity.kt
@@ -5,12 +5,33 @@ import android.app.Activity
 import com.catly.oledsaver.features.floating_window.FloatingWindowService
 
 // Wraps the service in an activity to be triggered by a shortcut
-class FloatingWindowServiceActivity : Activity() {
+class FloatingWindowServiceToggleActivity: Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (!FloatingWindowService.isRunning) {
             FloatingWindowService.startService(this)
         } else {
+            FloatingWindowService.stopService(this)
+        }
+        finish()
+    }
+}
+
+// Wraps the service in an activity to be triggered by a shortcut
+class FloatingWindowServiceOnActivity: Activity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!FloatingWindowService.isRunning) {
+            FloatingWindowService.startService(this)
+        }
+        finish()
+    }
+}
+// Wraps the service in an activity to be triggered by a shortcut
+class FloatingWindowServiceOffActivity: Activity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (FloatingWindowService.isRunning) {
             FloatingWindowService.stopService(this)
         }
         finish()
